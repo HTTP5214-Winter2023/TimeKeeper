47eb80423f1f25269a99a68c775f8c5ac2d99d92
Daniel So Ho Kit <58765171+Daniel320So@users.noreply.github.com>
2023-03-04 10:49:46 -0500

    Add utils.js to interact with Config.json

--- app/src/command_line_interface/index.js
+++ app/src/command_line_interface/index.js
@@ -1,24 +1,35 @@
 import inquirer from 'inquirer';
+import { readApiConfig, writeApiConfig } from '../utils.js';
 
-let API_KEY = ""; //For dev only, it should be returned from the api module
+let apiData;
+
+const APIKeyPrompt = async function () {
+  const APIKeyPrompt = inquirer.createPromptModule();
+  await APIKeyPrompt([
+    {
+      type: "input",
+      name: "key",
+      message: "Please provide your api key",
+    },
+  ])
+    .then(async (answers) => {
+      //Update API_KEY in JSON file
+      apiData.API_KEY = answers.key;
+      await writeApiConfig(apiData);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
 
 export async function startCli(){
 
-    //Ask the user to setup API key if it is not setup yet
-    if (!API_KEY || API_KEY === "") {
-        const APIKeyPrompt = inquirer.createPromptModule();
-        await APIKeyPrompt([{
-            type: "input",
-            name: "key",
-            message: "Please provide your api key"
-        }]).then((answers) => {
-            API_KEY = answers.key;
-        }).catch((error) => {
-            console.log(error)
-        });
-    };
+    apiData = await readApiConfig();
 
-    console.log("API_KEY:", API_KEY)
+    //Ask the user to setup API key if it is not setup yet
+    if (!apiData || !apiData.API_KEY || apiData.API_KEY === "") {
+        await APIKeyPrompt();
+    };
 
     //Ask the user for the next actions
     const actionPrompt = inquirer.createPromptModule();
