b74d91588ec4a7c147bf73749cc5c213a6e5c73c
Mary Louise Anhance Abrena <abrenalouise@gmail.com>
2023-03-21 19:03:11 -0400

    Squashed commit of the following:
    commit 54991e3fb0188848d2f7a1879e2a6d2936b1a61c
    Merge: a53471e df9ace4
    Author: Daniel So Ho Kit <58765171+Daniel320So@users.noreply.github.com>
    Date:   Mon Mar 20 21:03:34 2023 -0400
    
        Merge pull request #91 from HTTP5214-Winter2023/WL-Kevin-Lu
    
        CLI 'add new project', 'add new task', debug API
    
    commit df9ace485298ec3c993f1f6dff1d31895ad39100
    Author: wenhao-lu <wenhaolu.kevin@gmail.com>
    Date:   Mon Mar 20 20:26:29 2023 -0400
    
        CLI 'add new project', 'add new task', debug API
    
    commit a53471e555aadaa1063df83da3765dfb14390e87
    Merge: 42546c3 3660051
    Author: wenhaolu <52440282+wenhao-lu@users.noreply.github.com>
    Date:   Mon Mar 20 17:42:44 2023 -0400
    
        Merge pull request #90 from HTTP5214-Winter2023/HoKitSo
    
        Add startTimerFromCli and Fix StopTimer display
    
    commit 36600519bae2b5259d5fa3cefb5172bd29e11f4b
    Author: Daniel So Ho Kit <58765171+Daniel320So@users.noreply.github.com>
    Date:   Mon Mar 20 17:10:21 2023 -0400
    
        Remove duplicate files and codes
    
    commit ed4327238444f9f9635a6600e66b620b5c60bb1f
    Author: Daniel So Ho Kit <58765171+Daniel320So@users.noreply.github.com>
    Date:   Mon Mar 20 17:07:05 2023 -0400
    
        Fix stop timer display issue
    
    commit ef98c1288447bd83696161ddb2f038f2d8ab386b
    Merge: c699e99 42546c3
    Author: Daniel So Ho Kit <58765171+Daniel320So@users.noreply.github.com>
    Date:   Mon Mar 20 17:00:04 2023 -0400
    
        Merge branch 'main' into HoKitSo
    
    commit 42546c32b996e2e665e04ac44eea5fd0c30b2150
    Merge: a5faa20 8ec620c
    Author: Daniel So Ho Kit <58765171+Daniel320So@users.noreply.github.com>
    Date:   Mon Mar 20 16:58:18 2023 -0400
    
        Merge pull request #89 from HTTP5214-Winter2023/MaryLouiseAbrena
    
        added getTimeentries + message when timer has stopped
    
    commit a5faa2077498db2d697eb59bf2f8445b5b4f2d83
    Merge: 546a5f9 1b18294
    Author: Daniel So Ho Kit <58765171+Daniel320So@users.noreply.github.com>
    Date:   Mon Mar 20 16:57:33 2023 -0400
    
        Merge pull request #88 from HTTP5214-Winter2023/raymond
    
        fixed timestamp format
    
    commit 1b18294cd2537e99c5db3a7996adfbb8e69f013d
    Author: xRayMonDxp <raymond980711@gmail.com>
    Date:   Mon Mar 20 14:52:01 2023 -0400
    
        Fix timestamp format
    
    commit 0b05ac37412324c404dc2365f14ce43642fc59ce
    Author: xRayMonDxp <raymond980711@gmail.com>
    Date:   Mon Mar 20 14:09:20 2023 -0400
    
        format code
    
    commit c699e99dc8b2ffecdfe65a4633c332f0b24ff0df
    Merge: e45c5f1 93288d3
    Author: Daniel So Ho Kit <58765171+Daniel320So@users.noreply.github.com>
    Date:   Sat Mar 18 20:05:22 2023 -0400
    
        Merge branch 'main' into HoKitSo
    
    commit e45c5f1ceea86b33524012a4a72ac67d42fba183
    Author: Daniel So Ho Kit <58765171+Daniel320So@users.noreply.github.com>
    Date:   Sat Mar 18 15:14:43 2023 -0400
    
        Add startTimerFromCli

--- app/src/cli.js
+++ app/src/cli.js
@@ -7,6 +7,8 @@ import {
   getTimeentries,
   startTimer,
   stopTimer,
+  addNewProject,
+  addTask
 } from "./api.js";
 import {
   readApiConfig,
@@ -14,6 +16,9 @@ import {
   formatDuration,
   timeConvert,
 } from "./utils.js";
+import{
+  createTimesheet,
+} from "./report.js";
 
 const ACTIONS = {
   EXPORT_EXCEL: "exportExcel",
@@ -25,6 +30,17 @@ const ACTIONS = {
 };
 
 let config;
+
+const checkAPIConfig = async function () {
+  config = await readApiConfig();
+  //Ask the user to setup API key if it is not setup yet
+  if (!config || !config.API_KEY || config.API_KEY === "") {
+    console.log(
+      "You may visit https://app.clockify.me/user/settings to get your api Key."
+    );
+    await callAPIKeyPrompt();
+  }
+};
 
 const callAPIKeyPrompt = async function () {
   const APIKeyPrompt = inquirer.createPromptModule();
@@ -140,8 +156,10 @@ const callStartTimerPrompt = async function () {
         message: "What is the new project name?",
       },
     ])
+      // call api to add new project
       .then(async (answers) => {
-        // call api to add new project
+        const newProject = await addNewProject(answers.projectName);
+        selectedProject = newProject;
       })
       .catch((error) => {
         console.log(error);
@@ -182,8 +200,10 @@ const callStartTimerPrompt = async function () {
         message: "What is the new task name?",
       },
     ])
+      // call api to add new task
       .then(async (answers) => {
-        // call api to add new task
+        const newTask = await addTask(selectedProject.id, answers.taskName);
+        selectedTask = newTask;
       })
       .catch((error) => {
         console.log(error);
@@ -214,21 +234,19 @@ const callStartTimerPrompt = async function () {
 };
 
 export async function startCli() {
-  config = await readApiConfig();
 
   //Ask the user to setup API key if it is not setup yet
-  if (!config || !config.API_KEY || config.API_KEY === "") {
-    console.log(
-      "You may visit https://app.clockify.me/user/settings to get your api Key."
-    );
-    await callAPIKeyPrompt();
-  }
+  await checkAPIConfig();
 
   //Choiced for action prompt:
   let choices = [
     {
       name: "Start a New Timer",
       value: ACTIONS.START_TIMER,
+    },
+    {
+      name: "Stop Current Timer",
+      value: ACTIONS.STOP_CURRENT_TIMER,
     },
     {
       name: "Check Projects List",
@@ -249,22 +267,23 @@ export async function startCli() {
   ];
 
   // Check whether there is running timer.
-  // 1. Get all time entreis (getClockifyData)
-  let haveRunningTimer = true;
-  let timeentries = await getTimeentries();
-  // 2. check on each one to see whether there is an entry without "end" value
-  console.log(timeentries);
+  let haveRunningTimer = false;
+  let runningTimeEntry;
+  let timeEntries = await getTimeentries();
 
-  for (const entry of timeentries) {
-  }
+  for (const entry of timeEntries) {
+    // If there is a record without end value
+    if (entry.timeInterval.end == null) {
+      haveRunningTimer = true;
+      runningTimeEntry = entry;
+    } 
+  };
 
-  // if (duration === null && endTime === null) {
   if (haveRunningTimer) {
-    choices.push({
-      name: "Stop Current Timer",
-      value: ACTIONS.STOP_CURRENT_TIMER,
-    });
-  }
+    choices = choices.filter( c => c.value !== ACTIONS.START_TIMER);
+  } else {
+    choices = choices.filter( c => c.value !== ACTIONS.STOP_CURRENT_TIMER);
+  };
 
   //Ask the user for the next actions
   let action;
@@ -291,7 +310,7 @@ export async function startCli() {
       break;
     case ACTIONS.STOP_CURRENT_TIMER:
       await stopTimer(); //API stop the running timer.
-      console.log("The timer has now stopped.");
+      console.log(`The timer has now stopped for ${runningTimeEntry.description} .`);
       break;
     case ACTIONS.CHECK_PROJECTS:
       await callProjectPrompt();
@@ -310,4 +329,38 @@ export async function startCli() {
 
   // Back to the menu
   await startCli();
+}
+
+export async function startTimerFromCli(projectName, taskName, description) {
+
+  await checkAPIConfig();
+
+  //Find Project
+  const projects = await getProjects();
+  if ( !projects || projects == 0) {
+    console.log("No project is found");
+    return;
+  };
+  const project = projects.find( p => p.name == projectName);
+  if (!project) {
+    console.log(`${projectName} is not found.`);
+    return;
+  } 
+
+  //Find Task
+  const tasks = await getTasks(project.id);
+  if ( !tasks || tasks == 0) {
+    console.log(`No tasks are found in ${project.name}`);
+    return;
+  };
+  const task = tasks.find( t => t.name == taskName);
+  if (!task) {
+    console.log(`${taskName} is not found.`);
+    return;
+  } 
+
+  await startTimer(description, project.id, task.id);
+  console.log(
+    `A timer has been started for ${description} on ${project.name} - ${task.name}`
+  );
 }
